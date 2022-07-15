032f67d5798f43e5d957c18973806787666bfc85
Ivan <39524682+Skrals@users.noreply.github.com>
2022-07-15 18:21:56 +0300

    Game timer

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    [SerializeField] private GameObject _game;
     [SerializeField] private UIControl _uIControl;
+    [SerializeField] private GameObject _game;
+    [SerializeField] private GameTimer _timer;
 
-    private void OnEnable() => _uIControl.StartGame += OnStartGame;
+    private void OnEnable()
+    {
+        _uIControl.StartGame += OnStartGame;
+        _timer.GameOver += OnGameOver;
+    }
 
-    private void OnDisable() => _uIControl.StartGame -= OnStartGame;
+    private void OnDisable()
+    {
+        _uIControl.StartGame -= OnStartGame;
+        _timer.GameOver -= OnGameOver;
+    }
 
     private void OnStartGame(bool start)
     {
         _game.SetActive(start);
+        _timer.StartTimer();
+    }
+
+    private void OnGameOver(bool over)
+    {
+       
     }
 }
---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameTimer : MonoBehaviour
+{
+    public event UnityAction<bool> GameOver;
+
+    [SerializeField] private float _levelTime;
+    [SerializeField] private float _currentTimer;
+
+    private bool _gameOver;
+
+    public void StartTimer()
+    {
+        _currentTimer = _levelTime;
+        _gameOver = false;
+    }
+
+    private void Update()
+    {
+        if (_gameOver)
+        {
+            return;
+        }
+
+        if (_currentTimer > 0)
+        {
+            _currentTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _gameOver = true;
+            GameOver?.Invoke(_gameOver);
+        }
+    }
+}
