bdfcc7bfc549a37f99d9e11b28c9de1501c38d4f
Ivan <39524682+Skrals@users.noreply.github.com>
2022-07-17 16:58:23 +0300

    Matching

--- Assets/Scripts/Board/BoardController.cs
+++ Assets/Scripts/Board/BoardController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using DG.Tweening;
 
 public class BoardController : Board
 {
-    private Tile _oldSelectionTile;
-    private Tile _oldCashSelected;
-    private Vector2[] _directionRay = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+    protected Tile _oldSelectionTile;
+    protected Tile _oldCashSelected;
+    protected Vector2[] _directionRay = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+    protected bool isFindMatch = false;
 
     private void Update()
     {
@@ -24,22 +26,7 @@ public class BoardController : Board
             }
         }
     }
-
-    private void Selected(Tile tile)
-    {
-        tile.isSelected = true;
-        tile.SpriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
-        _oldSelectionTile = tile;
-    }
-
-    private void DeselectTile(Tile tile)
-    {
-        tile.isSelected = false;
-        tile.SpriteRenderer.color = new Color(1, 1, 1);
-        _oldSelectionTile = null;
-    }
-
-    private void ChekSelectTile(Tile tile)
+    private async void ChekSelectTile(Tile tile)
     {
         if (tile.isEmpty)
         {
@@ -58,9 +45,13 @@ public class BoardController : Board
             }
             else
             {
-                if(NeighbourTile().Contains(tile))
+                if (NeighbourTile().Contains(tile))
                 {
                     SwapTile(tile);
+                    await Task.Delay(1000);
+
+                    FindAllMatch(tile);
+                    FindAllMatch(_oldSelectionTile);
                     DeselectTile(_oldSelectionTile);
                 }
                 else
@@ -72,6 +63,25 @@ public class BoardController : Board
         }
     }
 
+    #region tile selection/deselection
+
+    private void Selected(Tile tile)
+    {
+        tile.isSelected = true;
+        tile.SpriteRenderer.color = new Color(0.5f, 0.5f, 0.5f);
+        _oldSelectionTile = tile;
+    }
+
+    private void DeselectTile(Tile tile)
+    {
+        tile.isSelected = false;
+        tile.SpriteRenderer.color = new Color(1, 1, 1);
+        _oldSelectionTile = null;
+    }
+
+    #endregion
+
+    #region swap tiles
     private void SwapTile(Tile tile)
     {
         if (_oldSelectionTile.SpriteRenderer.sprite == tile.SpriteRenderer.sprite)
@@ -94,13 +104,70 @@ public class BoardController : Board
 
         for (int i = 0; i < _directionRay.Length; i++)
         {
-            RaycastHit2D hit = Physics2D.Raycast(_oldSelectionTile.transform.position,_directionRay[i]);
+            RaycastHit2D hit = Physics2D.Raycast(_oldSelectionTile.transform.position, _directionRay[i]);
 
-            if(hit.collider != null )
+            if (hit.collider != null)
             {
                 neighbours.Add(hit.collider.gameObject.GetComponent<Tile>());
             }
         }
         return neighbours;
     }
+
+    #endregion
+
+    #region Mathc3 and delete matching
+
+    private List<Tile> FindMatch(Tile start, Vector2 direction)
+    {
+        List<Tile> findTiles = new List<Tile>();
+        RaycastHit2D hit = Physics2D.Raycast(start.transform.position, direction);
+
+        while (hit.collider != null && hit.collider.gameObject.GetComponent<Tile>().SpriteRenderer.sprite == start.SpriteRenderer.sprite)
+        {
+            findTiles.Add(hit.collider.gameObject.GetComponent<Tile>());
+            hit = Physics2D.Raycast(hit.collider.gameObject.transform.position, direction);
+        }
+
+        return findTiles;
+    }
+
+    private void DeleteSprite(Tile tile, Vector2[] dirArray)
+    {
+
+        List<Tile> findTiles = new List<Tile>();
+
+        for (int i = 0; i < dirArray.Length; i++)
+        {
+            findTiles.AddRange(FindMatch(tile, dirArray[i]));
+        }
+
+        if (findTiles.Count >= 2)
+        {
+            for (int i = 0; i < findTiles.Count; i++)
+            {
+                findTiles[i].SpriteRenderer.sprite = null;
+            }
+            isFindMatch = true;
+        }
+    }
+
+    private void FindAllMatch(Tile tile)
+    {
+        if (tile.isEmpty)
+        {
+            return;
+        }
+
+        DeleteSprite(tile, new Vector2[2] { Vector2.up, Vector2.down });
+        DeleteSprite(tile, new Vector2[2] { Vector2.left, Vector2.right }); ;
+
+        if (isFindMatch)
+        {
+            tile.SpriteRenderer.sprite = null;
+            isFindMatch = false;
+        }
+    }
+
+    #endregion
 }
